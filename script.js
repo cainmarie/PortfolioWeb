de0c03a2e278c586ea9badf5edfa78a7d6c17263
Selena Brown <cainmarie@icloud.com>
2023-10-18 13:36:10 -0400

    updated navbar highlight and video width

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//FOOTER YEAR CHANGE
 const year = document.querySelector('#current-year')
-
 year.innerHTML = new Date().getFullYear()
+
+//ACTIVE NAV BUTTON
+document.addEventListener("DOMContentLoaded", function() {
+    var currentUrl = window.location.href;
+    var navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function(link) {
+        if (link.href === currentUrl) {
+            link.classList.add('active-nav');
+        }
+    });
+});
